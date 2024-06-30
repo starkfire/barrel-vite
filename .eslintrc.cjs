2f45cd2cbed54c36a1ab4547452ae946a4898d57
starkfire <plpantoja49@gmail.com>
2024-06-30 09:30:13 +0800

    chore: add avoid-importing-barrel-files rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,17 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'barrel-files'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'barrel-files/avoid-importing-barrel-files': [
+      'warn',
+      {
+        debug: true
+      }
     ],
   },
 }
